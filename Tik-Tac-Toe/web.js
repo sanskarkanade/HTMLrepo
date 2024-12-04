592d2f8c6da18bf29a46178af267777c0e622453
sanskarkanade <kanadesanskar79@gmail.com>
2024-12-04 22:06:42 +0530

    text added

--- Tik-Tac-Toe/web.js
+++ Tik-Tac-Toe/web.js
@@ -1,9 +1,19 @@
-let boxes = document.querySelectorAll(".box")
+let boxes = document.querySelectorAll(".box");
 
-let turn = 0
+let turn = 1
 boxes.forEach((box) => {
     box.addEventListener("click" , ()=>
     {
-        console.log("ok")
+        if(turn)
+        {
+            box.innerText = "X";
+            turn = 0;
+        }
+        else
+        {
+            box.innerText = "O";
+            turn = 1;
+        }
+        box.disabled = true;
     })
 });
