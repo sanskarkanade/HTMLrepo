b64750ec9d433a9193d272974dd3199cf229a76c
sanskarkanade <kanadesanskar79@gmail.com>
2024-12-05 21:00:40 +0530

    win

--- Tik-Tac-Toe/web.js
+++ Tik-Tac-Toe/web.js
@@ -1,5 +1,18 @@
 let boxes = document.querySelectorAll(".box");
 
+const winpattern = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+let count = 0;
+
 let turn = 1
 boxes.forEach((box) => {
     box.addEventListener("click" , ()=>
@@ -15,5 +28,25 @@ boxes.forEach((box) => {
             turn = 1;
         }
         box.disabled = true;
+        count++;
+
+         checkwinner();
     })
 });
+
+const checkwinner = () => {
+    for(let pattern of winpattern)
+    {
+        let pos1 = boxes[pattern[0]].innerText;
+        let pos2 = boxes[pattern[1]].innerText;
+        let pos3 = boxes[pattern[2]].innerText;
+
+        if(pos1 != "" && pos2 != "" && pos3 != "")
+        {
+            if(pos1 === pos2 && pos2 === pos3)
+            {
+                console.log("win");
+            }
+        }
+    }
+};
