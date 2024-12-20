aaf051a507b66e3c7fc96f4838e4616eccf293cd
sanskarkanade <kanadesanskar79@gmail.com>
2024-12-20 12:21:11 +0530

    completed

--- Todo list/web.js
+++ Todo list/web.js
@@ -4,13 +4,25 @@ let showtodo = document.querySelector(".showtodo");
 
 
 addbtn.addEventListener("click" , () => {
+    if(text.value != "")
+    {
     let element = document.createElement("div")
     let delbtn = document.createElement("button")
     let checkbox = document.createElement("input")
 
     checkbox.setAttribute("type" , "checkbox")
+    checkbox.setAttribute("id" , "todo")
+
+
 
     delbtn.innerText = "Delete"
+    delbtn.setAttribute("class" , "dbtn")
+
+    
+       
+
+    element.setAttribute("class" , "eachtodo")
+
 
     delbtn.addEventListener("click" , () => {
         showtodo.removeChild(element)
@@ -23,4 +35,6 @@ addbtn.addEventListener("click" , () => {
     element.appendChild(delbtn)
     showtodo.appendChild(element)
 
-})
+    text.value = ""
+
+}})
